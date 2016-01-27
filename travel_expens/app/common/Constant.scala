8b0590900ec92d461747fc2bc7bc19d385db3fb2
peko-the3rd <mtennti.the3rd@gmail.com>
2016-01-27 10:17:00 +0900

    Update Constant.scala
    名前がおかしい

--- travel_expens/app/common/Constant.scala
+++ travel_expens/app/common/Constant.scala
@@ -4,7 +4,7 @@ class Constant {
   
 }
 object TeConstant{
-    val FULL_NAME = "田中一貴"
+    val FULL_NAME = "テスト与太郎"
     val DETAIL_LINES = 10
     val CLASSIFICATION_S = List(Map('cd -> "1", 'name -> "日帰り"),Map('cd -> "2", 'name -> "出張"))
     
